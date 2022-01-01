c09f5abb6199f781f42ad8646c90b5cbf31666e4
Gabriel Bertollo <gabrielbertollo.gb@gmail.com>
2022-01-01 20:16:20 -0300

    feat: added task info to lists

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -10,7 +10,7 @@ router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
     try {
-        const projects = await Project.find().populate('user');
+        const projects = await Project.find().populate(['user', 'tasks']);
 
         return res.send({ projects });
     } catch (error) {
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:projectId', async (req, res) => {
     try {
-        const project = await Project.findById(req.params.projectId).populate('user');
+        const project = await Project.findById(req.params.projectId).populate(['user', 'tasks']);
 
         return res.send({ project });
     } catch (error) {
